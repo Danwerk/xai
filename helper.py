db01486f2787c7b6684d4d886e408bdcd265f221
Danyil Kurbatov <dakurb@taltech.ee>
2025-04-26 00:57:42 +0300

    Comparison of different models.

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolu
 import numpy as np
 import matplotlib.pyplot as plt
 
-def evaluate_model(y_true, y_pred, time_period, name="Model Name", show_plot=True):
+def evaluate_model(y_true, y_pred, time_period, name="Model Name", show_plot=False):
     """Evaluate model by calculating different metrics."""
     mae = mean_absolute_error(y_true, y_pred)
     mse = mean_squared_error(y_true, y_pred)
@@ -20,11 +20,11 @@ def evaluate_model(y_true, y_pred, time_period, name="Model Name", show_plot=Tru
     if show_plot:
         plt.figure(figsize=(15, 5))
         if time_period is not None:
-            plt.plot(time_period, y_true, label='Tegelik', color='black')
-            plt.plot(time_period, y_pred, label='Prognoos', linestyle='--', color='orange')
+            plt.plot(time_period, y_true, label='Actual', color='black')
+            plt.plot(time_period, y_pred, label='Forecast', linestyle='--', color='orange')
         else:
-            plt.plot(y_true, label='Tegelik', color='black')
-            plt.plot(y_pred, label='Prognoos', linestyle='--', color='orange')
+            plt.plot(y_true, label='Actual', color='black')
+            plt.plot(y_pred, label='Forecast', linestyle='--', color='orange')
 
         plt.title(name)
         plt.xlabel('Aeg')
@@ -33,6 +33,7 @@ def evaluate_model(y_true, y_pred, time_period, name="Model Name", show_plot=Tru
         plt.grid(True)
         plt.tight_layout()
         plt.show()
+    return {'Model': name, 'MAE': round(mae, 2), 'MAPE': round(mape, 2), 'RMSE': round(rmse, 2), 'R2': round(r2, 2)}
     #return {'MAE': mae, 'MAPE': mape, 'MSE': mse, 'RMSE': rmse, 'R2': r2}
 
 def evaluate_all_cases(y_test, y_pred, time_index, model_name="Model"):
@@ -40,23 +41,27 @@ def evaluate_all_cases(y_test, y_pred, time_index, model_name="Model"):
     start = "2019-01-17 14:00"
     end = "2019-01-23 20:00"
     mask = (time_index >= start) & (time_index <= end)
-    evaluate_model(
+
+    res_1 = evaluate_model(
         y_true=y_test.loc[mask],
         y_pred=y_pred[mask],
         time_period=time_index.loc[mask],
         name=f"{model_name} (17.01–23.01)"
     )
 
-    evaluate_model(
+
+    res_2 = evaluate_model(
         y_true=y_test.iloc[:200],
         y_pred=y_pred[:200],
         time_period=time_index.iloc[:200],
-        name=f"{model_name} – esimesed 200 tundi"
+        name=f"{model_name} (first 200 hours)"
     )
 
-    evaluate_model(
+    res_3 = evaluate_model(
         y_true=y_test,
         y_pred=y_pred,
         time_period=time_index,
-        name=f"{model_name} – kõik testandmed"
+        name=f"{model_name} (all dataset)"
     )
+
+    return res_1, res_2, res_3
